bf86cc4906958256cd369b1b8523384babb14a16
alex jimenez <ajimenez@student.42madrid.com>
2021-07-28 17:53:38 +0200

    libft

--- libft/mem/ft_memset.c
+++ libft/mem/ft_memset.c
@@ -3,23 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ajimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/27 12:48:29 by apuchill          #+#    #+#             */
-/*   Updated: 2020/02/19 14:04:55 by apuchill         ###   ########.fr       */
+/*   Created: 2021/07/28 17:28:50 by ajimenez          #+#    #+#             */
+/*   Updated: 2021/07/28 17:43:54 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** LIBRARY: <string.h>
-** SYNOPSIS: write a byte to a byte string
-**
-** DESCRIPTION:
-** 		The memset() function writes n bytes of value c (converted to an
-**	unsigned char) to the string s.
-*/
-
-#include "libft.h"
+#include <stdlib.h>
 
 void	*ft_memset(void *b, int c, size_t len)
 {
