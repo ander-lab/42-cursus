82b5249f2b739155956618efb6cc2765ac3271df
Alex <jimenezs.alex@gmail.com>
2021-08-04 12:58:13 +0200

    atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 14:42:33 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/08/04 12:44:16 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/08/04 12:57:36 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,18 @@ int	ft_atoi(const char *str)
 	int	i;
 	int	s;
 	int	r;
-	int	count;
 
 	i = 0;
 	s = 1;
 	r = 0;
-	count = 0;
-	while ( str[i] <= 13 || str[i] == ' ')
+	while (str[i] <= 13 || str[i] == ' ')
 		i++;
-	while (str[i] == 45 || str[i] == 43)
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (str[i] == 45)
+		if (str[i] == '-')
 			s = -1;
 		i++;
-		count++;
 	}
-	if (count > 1)
-		return (0);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		r = (r * 10) + (str[i] - '0');
